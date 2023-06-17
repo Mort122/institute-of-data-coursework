53eb3eb957670de10ee96a3fa733b9cdea71f3de
Garrett Palma <palmagarrett@gmail.com>
2023-06-17 13:56:26 -0500

    Math js Commit

---
+++ module-1/math.js
@@ -0,0 +1,40 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function sayHello(name) {
+    const greeting = 'Hello';
+    const greetingString = greeting + " " + name;
+    return greetingString
+}
+
+
+console.log(8+9);
+console.log(4-3);
+console.log(4/2);
+console.log(3*4);
+
+console.log(sayHello('Garrett'));
+
+
+
+
+
+
+
+
+
+
+
