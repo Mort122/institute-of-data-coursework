ec76a2bee8e8b51c6f02462a93024b8c3a0148f7
Garrett Palma <palmagarrett@gmail.com>
2023-07-15 12:10:13 -0500

    add module-1 content

---
+++ module-1/nodePractice/math.js
@@ -0,0 +1,49 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function sayHello(name) {
+    const greeting = 'Hello';
+    const greetingString = greeting + " " + name;
+    return greetingString
+}
+
+
+console.log(8+9);
+console.log(4-3);
+console.log(4/2);
+console.log(3*4);
+
+console.log(sayHello('Garrett'));
+
+var Sentiment = require('sentiment');
+var sentiment = new Sentiment();
+var result = sentiment.analyze('Cats are stupid.');
+console.dir(result);    // Score: -2, Comparative: -0.666
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
