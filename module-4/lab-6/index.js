9dd81277eb27816ba52a0b5791d22527f1110fab
unknown <palmagarrett@gmail.com>
2023-07-29 16:56:45 -0500

    question 2

--- module-4/lab-6/index.js
+++ module-4/lab-6/index.js
@@ -6,3 +6,20 @@ const birthday = moment('2023-12-20');
 const days = birthday.diff(today, 'days');
 
 console.log(days);
+
+
+//Excercise B
+
+const moment = require('moment');
+const birthday = moment('1994-12-20');
+const today = moment();
+
+const years = today.diff(birthday, 'years');
+birthday.add(years, 'years');
+
+const months = today.diff(birthday, 'months');
+birthday.add(months, 'months');
+
+const days = today.diff(birthday, 'days');
+
+console.log(years + ' years ' + months + ' months ' + days + ' days ');
