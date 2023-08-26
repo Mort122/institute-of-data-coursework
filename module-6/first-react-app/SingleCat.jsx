27517fc89780f2d405a2af1741d1980194842b60
Garrett Palma <palmagarrett@gmail.com>
2023-08-26 15:41:35 -0500

    Completed BigCats Exercises

--- module-6/first-react-app/SingleCat.jsx
+++ module-6/first-react-app/SingleCat.jsx
@@ -1,11 +1,16 @@
-function SingleCat({ cat }) {
+function SingleCat({ cat, onDelete }) {
     return (
-        <div className="single-cat">
-            <img src={cat.image}/>
-            <h1>{cat.name}</h1>
+        <div className="cat-container">
+            <img src={cat.image} alt={cat.name} />
+            <h2>{cat.name}</h2>
             <p>{cat.latinName}</p>
+            <a href="#" onClick={e => {
+                e.preventDefault();
+                onDelete();
+            }}>Delete</a>
         </div>
     );
 }
 
 export default SingleCat;
+
